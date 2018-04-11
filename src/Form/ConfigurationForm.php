04cf745f61d13c7e7880e4cc659558c3ef0913dc
Unknown <ProFire88@hotmail.com>
2018-04-11 18:12:36 +0800

    Enhancement: ADS settings implemented
    Including a configuration form to change the settings

--- src/Form/ConfigurationForm.php
+++ src/Form/ConfigurationForm.php
@@ -1,27 +1,29 @@
 <?php
 namespace Drupal\authorized_digital_sellers\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-class ConfigurationForm extends FormBase {
+class ConfigurationForm extends ConfigFormBase {
   public function getFormId() {
     return "authorized_digital_sellers_configuration_form";
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config("authorized_digital_sellers.settings");
     $form["description"] = [
       "#type" => "item",
       "#markup" => $this->t("Authorized Digital Seller configuration form"),
     ];
 
-    $form["external_self_radio"] = [
+    $form["radio_external_self"] = [
       "#type" => "radios",
       "#title" => $this->t("External File Management or Self-Managed"),
       "#options" => [
         "external" => $this->t("External File Management"),
         "self" => $this->t("Self-Managed"),
       ],
+      "#default_value" => $config->get("radio_external_self"),
     ];
 
     $form["external_ads_file"] = [
@@ -30,20 +32,22 @@ class ConfigurationForm extends FormBase {
       "#description" => $this->t("Enter the http/https location of the ads.txt file to retrieve, if you selected External File Management."),
       "#required" => false,
       "#placeholder" => "https://example/ads.txt",
+      "#default_value" => $config->get("external_ads_file"),
     ];
 
-    $form["self_managed"] = [
+    $form["self_managed_text"] = [
       "#type" => "textarea",
       "#title" => $this->t("Self Managed ADS text file"),
       "#description" => $this->t("Enter the ads.txt information, if you selected Self-Managed."),
       "#required" => false,
+      "#default_value" => $config->get("self_managed_text"),
     ];
 
     $form["actions"] = [
       "#type" => "actions",
       "submit" => [
         "#type" => "submit",
-        "#value" => $this->t("Push this Button"),
+        "#value" => $this->t("Save"),
       ],
     ];
 
@@ -54,5 +58,21 @@ class ConfigurationForm extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $config = \Drupal::service('config.factory')->getEditable("authorized_digital_sellers.settings");
+
+    $config->set("radio_external_self", $form_state->getValue("radio_external_self"));
+    $config->set("external_ads_file", $form_state->getValue("external_ads_file"));
+    $config->set("self_managed_text", $form_state->getValue("self_managed_text"));
+
+    //Save Configuration
+    $config->save();
+  }
+
+  protected function getEditableConfigNames() {
+    return [
+      "authorized_digital_sellers.settings",
+    ];
   }
 }
